b47e3e351c93245648dddb6c8683465026383efa
ivilkin <ivilkin@cern.ch>
2019-09-17 16:59:54 +0200

    Modified for Bc and Bcstar

--- src/JPsiKaon.cc
+++ src/JPsiKaon.cc
@@ -361,13 +361,13 @@ void JPsiKaon::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 		   reco::TransientTrack kaonTT((*theB).build(iTrack1->pseudoTrack()));
                    
-		   ParticleMass kaon_mass = 0.493677;
+		   ParticleMass kaon_mass = 0.1395706;
                    TLorentzVector kaon14V, p4_jpsi, p4chi0;
 		   kaon14V.SetXYZM(iTrack1->px(),iTrack1->py(),iTrack1->pz(),kaon_mass);
                    p4_jpsi.SetXYZM(psi_vFit_noMC->currentState().globalMomentum().x(),psi_vFit_noMC->currentState().globalMomentum().y(),psi_vFit_noMC->currentState().globalMomentum().z(),psi_vFit_noMC->currentState().mass());
 		   
 		   p4chi0 = p4_jpsi + kaon14V;
-		   if (p4chi0.M() < 4.3 || p4chi0.M() > 6.3) continue;
+		   if (p4chi0.M() < 5.3 || p4chi0.M() > 7.3) continue;
 
 
 
@@ -504,7 +504,7 @@ void JPsiKaon::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
         if (!bCandMC->currentState().isValid()) continue;
         if (!bDecayVertexMC->vertexIsValid())  continue;
 
-		    if ( (bCandMC->currentState().mass() < 5.16) || (bCandMC->currentState().mass() > 5.44) ) {
+		    if ( (bCandMC->currentState().mass() < 6.16) || (bCandMC->currentState().mass() > 6.44) ) {
 		      continue;
 		    }
 
